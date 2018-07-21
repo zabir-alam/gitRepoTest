10a2d4c139cdf8c1263dcc3ba743884e7caef5ea
zabir-alam <36799142+zabir-alam@users.noreply.github.com>
2018-07-21 16:45:19 +0530

    added new constant calss and sum change in app class

--- src/main/java/com/alam/GitProj/App.java
+++ src/main/java/com/alam/GitProj/App.java
@@ -7,12 +7,17 @@ package com.alam.GitProj;
 public class App 
 {
 	public void addData(String data1,String data2) {
+		if(data1.equalsIgnoreCase(Constant.DATA_TYPE)) {
 		System.out.println("your data1 is "+data1);
+		}
+		else {
 		System.out.println("your data2 is "+data2);
+		}
 		
 	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Changes in main method " );
     }
 }
---
+++ src/main/java/com/alam/GitProj/Constant.java
@@ -0,0 +1,5 @@
+package com.alam.GitProj;
+
+public class Constant {
+public static String DATA_TYPE="Zabir_Alam";
+}
