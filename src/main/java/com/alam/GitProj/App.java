4275dcd52526f2ff7858730b6ddb70587284ee5e
zabir-alam <36799142+zabir-alam@users.noreply.github.com>
2018-07-21 16:15:45 +0530

    added gitProj to local repository

---
+++ src/main/java/com/alam/GitProj/App.java
@@ -0,0 +1,16 @@
+package com.alam.GitProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data) {
+		System.out.println("your data is "+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
